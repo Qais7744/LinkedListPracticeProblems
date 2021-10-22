fe6411fdcc05bbc0d2dd9a102607704fc4eeba62
unknown <altamashbagwan1@gmail.com>
2021-10-23 01:30:45 +0530

    Added: Functioning of Insert value in between two value

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -33,6 +33,12 @@ public class MyLinkedList {
 			this.tail = myNode;
 		}
 	}
+	
+	public void insert(INode myNode, INode newNode) {
+		INode tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
 
 	public void printMyNodes() {
 		StringBuffer myNodes = new StringBuffer("My Nodes: ");
--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -6,17 +6,17 @@ import org.junit.Test;
 public class MyLinkedListTest {
 	
 	@Test
-	public void given3NumberWhenAppendedshouldBeAddedToLast() {
+	public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.add(myFirstNode);
-		myLinkedList.append(mySecondNode);
 		myLinkedList.append(myThirdNode);
+		myLinkedList.insert(myFirstNode, mySecondNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) && 
-				         myLinkedList.head.getNext().equals(mySecondNode) &&
+		boolean result = myLinkedList.head.equals(myFirstNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
 						 myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}

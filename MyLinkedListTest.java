8c1bd7c232f071d9a4bbfa6055952c26d825cb7c
unknown <altamashbagwan1@gmail.com>
2021-10-23 01:23:30 +0530

    Added: Functioning of Append value in between two value

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -4,19 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyLinkedListTest {
+	
 	@Test
-	public void given3NumberWhenAddedToLinkedListshouldBeAddedToTop() {
-		MyNode<Integer> myFirstNode = new MyNode<>(70);
+	public void given3NumberWhenAppendedshouldBeAddedToLast() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.add(myFirstNode);
-		myLinkedList.add(mySecondNode);
-		myLinkedList.add(myThirdNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myThirdNode)
-						 && myLinkedList.head.getNext().equals(mySecondNode)
-						 && myLinkedList.tail.equals(myFirstNode);
+		boolean result = myLinkedList.head.equals(myFirstNode) && 
+				         myLinkedList.head.getNext().equals(mySecondNode) &&
+						 myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
 }
